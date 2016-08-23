a8980858067116673700da03163f8a83ba86a0f8
susan <susan.ra.evans@gmail.com>
2016-08-22 22:37:54 -0700

    added hamming.rb

---
+++ 03-hamming/hamming.rb
@@ -0,0 +1,21 @@
+class Hamming
+# 	def initialize
+# 	end
+
+	def self.compute(strand1, strand2)
+		if(strand1.length != strand2.length)
+			raise ArgumentError.new
+		end
+
+		differences = 0
+		
+		for i in 0..strand1.length
+			if strand1[i] != strand2[i]
+				differences+=1
+			end
+		end
+		return differences
+	end
+end
+
+# Hamming.new
